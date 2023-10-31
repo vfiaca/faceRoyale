962a1964990e3363dda02c13ffa6f2b4aeecba28
kul-Devi <kunaldevi@ocadu.ca>
2023-10-30 22:11:18 -0400

    Code changes

--- mainGame.js
+++ mainGame.js
@@ -1,16 +1,30 @@
 let video;
 let canvas;
 
-function setup() {
-    canvas = createCanvas(windowWidth, windowWidth * 16 / 9);
+function preload() {
     video = createCapture(VIDEO);
     video.hide();
+}
 
+function setup() {
+    canvas = createCanvas(windowWidth, windowHeight);
     faceapi = ml5.faceApi(video, faceOptions, faceReady);
-
 }
 
 function draw() {
     image(video, 0, 0);
     drawBoxes(detections);
 }
+
+function Instructions() {
+    fill(230, 0, 0);
+    textSize(20);
+    noStroke();
+    text('Get your face in the frame or get eliminated!', windowWidth / 2, windowHeight / 2);
+}
+
+function BlackScreen() {
+    fill(0, 0, 0);
+    rect(0, 0, windowWidth, windowHeight);
+}
+
